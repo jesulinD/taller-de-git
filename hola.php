f1924adbd43c221b53624496884b9be3d2484beb
jesusmv <jmunvel2909@g.educaand.es>
2024-10-28 14:08:47 +0100

    Añadido el autor del programa y su email

--- hola.php
+++ hola.php
@@ -1,4 +1,5 @@
 <?php
-//Este comentario está mal
+//Autor: jesus <jesuspalen003@gmail.com>
+//El nombre por defecto es Mundo
 $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
 @print "Hola, {$nombre}\n";
